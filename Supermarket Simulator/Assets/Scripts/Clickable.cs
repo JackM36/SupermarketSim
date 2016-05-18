43b6bc4a68d485080403a741d15c1eae9a53b9aa
Gkokkinos <george1red@gmail.com>
2016-05-18 20:35:39 +0200

    UI_Basic
    Simple Buttons and color shelf

---
+++ Supermarket Simulator/Assets/Scripts/Clickable.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Clickable : MonoBehaviour {
+
+    string clickedShelf;
+    int clicked = 0;
+    GameObject[] allShelves;
+
+    void OnMouseDown()
+    {
+       GameObject[] allShelves = GameObject.FindGameObjectsWithTag("Shelve");
+        //make all shelfs white
+        for (int i =0; i<allShelves.Length; i++)
+        {
+            allShelves[i].GetComponentInChildren<MeshRenderer>().material.color = Color.white;
+        }
+        //make shelf red
+      gameObject.GetComponentInChildren<MeshRenderer>().material.color = Color.red ;
+    
+      //later
+        clickedShelf = gameObject.name;
+        
+        clicked = 1;
+       
+    }
+
+    public void OkButton()
+    {
+
+    }
+}
